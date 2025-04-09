09e92aa6a9449426bb131addcb3ead47a8211299
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2025-04-09 18:13:16 +0200

    Initial Commit

---
+++ EmotionsLabTherapist/EmotionsLabTherapistApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionsLabTherapistApp.swift
+//  EmotionsLabTherapist
+//
+//  Created by ahmed on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct EmotionsLabTherapistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmotionsLabTherapistTests/EmotionsLabTherapistTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionsLabTherapistTests.swift
+//  EmotionsLabTherapistTests
+//
+//  Created by ahmed on 09/04/2025.
+//
+
+import Testing
+@testable import EmotionsLabTherapist
+
+struct EmotionsLabTherapistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
